1c5fd5ac0b466324c8d3984cea2fd5ad9e974041
geovana <geovanasslima@gmail.com>
2024-11-03 01:50:47 -0300

    handle user sign up into database

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Platform } from 'react-native';
 
+import { AuthContext } from '../../contexts/auth';
 import { 
 	Background,
 	Container,
@@ -11,6 +12,16 @@ import {
  } from "../SignIn/styles";
 
 export default function SignUp() {
+	
+	const { signUp } = useContext(AuthContext)
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function handleSignUp() {
+		signUp(email, password, name);
+	}
+
 	return(
 		<Background>
 
@@ -22,22 +33,29 @@ export default function SignUp() {
 				<AreaInput>
 					<Input
 					 placeholder="Nome"
+					 value={name}
+					 onChangeText={ (text) => setName(text) }
 					/>
 				</AreaInput>
 
 				<AreaInput>
 					<Input
 					 placeholder="Seu email"
+					 value={email}
+					 onChangeText={ (text) => setEmail(text) }
 					/>
 				</AreaInput>
 
 				<AreaInput>
 					<Input
 					 placeholder="Sua senha"
+					 value={password}
+					 onChangeText={ (text) => setPassword(text) }
+					 secureTextEntry={true}
 					/>
 				</AreaInput>
 
-				<SubmitButton>
+				<SubmitButton onPress={handleSignUp}>
 					<SubmitText>Cadastrar</SubmitText>
 				</SubmitButton>
 
