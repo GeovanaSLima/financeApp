14fc3810bc9363e56524c0ade73d991916091379
geovana <geovanasslima@gmail.com>
2024-11-06 02:13:38 -0300

    creating Header and sstyling drawer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,13 +2,14 @@ import React, { useContext } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 
+import { Background } from "./styles";
+import Header from "../../components/Header";
+
 export default function Home() {
-  const { signOut, user } = useContext(AuthContext);
-  
+
   return(
-    <View >
-      <Text>Home {user.name}</Text>
-      <Button title="signn out" onPress={ () => signOut() } />
-    </View> 
+    <Background>
+      <Header title="Minhas Movimentações" />
+    </Background>
   )
 }
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
+import { ThemeProvider } from "styled-components";
+
 import Home from "../pages/Home";
+import { darkTheme } from "../assets/theme";
+
 
 const AppDrawer = createDrawerNavigator();
 
 function AppRoutes() {
+  const theme = darkTheme;
+
   return(
-    <AppDrawer.Navigator>
-      <AppDrawer.Screen
-        name="Home"
-        component={Home}
-      />
-    </AppDrawer.Navigator>
+    <ThemeProvider theme={theme}>
+      <AppDrawer.Navigator
+        screenOptions={{
+          headerShown: false,
+
+          drawerStyle: {
+            backgroundColor: theme.placeholder,
+            paddingTop: 20,
+          },
+
+          drawerActiveBackgroundColor: theme.primary,
+          drawerActiveTintColor: theme.background,
+
+          drawerInactiveBackgroundColor: theme.border,
+          drawerInactiveTintColor: theme.input
+        }}
+      >
+        <AppDrawer.Screen
+          name="Home"
+          component={Home}
+        />
+      </AppDrawer.Navigator>
+    </ThemeProvider>
   )
 }
 
