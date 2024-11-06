26630f85b8ca93b9c45999dc1b844beeb649dfe7
geovana <geovanasslima@gmail.com>
2024-11-05 23:46:40 -0300

    handling bearear to keep user logged in

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,14 +1,46 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import api from "../services/api";
 import { useNavigation } from "@react-navigation/native";
+
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoandingAuth] = useState(false);
+  const [loading, setLoanding] = useState(true);
 
   const navigation = useNavigation();
+
+  useEffect(() => {
+    async function loadStorage() {
+      const storageUser = await AsyncStorage.getItem('@finToken');
+
+      if (storageUser) {
+        const response = await api.get('/me', {
+          headers: {
+            'Authorization': `Bearer ${storageUser}`
+          }
+        })
+        .catch(() => {
+          setUser(null);
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+        
+        setUser(response.data);
+
+        setLoanding(false);
+
+      }
+
+      setLoanding(false);
+
+    }
+
+    loadStorage();
+  }, [])
 
   async function signUp(email, password, name) {
     setLoandingAuth(true);
@@ -47,6 +79,8 @@ function AuthProvider({ children }) {
         token,
         email,
       };
+
+      await AsyncStorage.setItem('@finToken', token)
 
       api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
