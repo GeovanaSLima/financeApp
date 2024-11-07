caf0383a7db53e9b6eac0f55af397a69a5db6a04
geovana <geovanasslima@gmail.com>
2024-11-07 00:05:21 -0300

    creating balance daiy movements cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
-import { AuthContext } from "../../contexts/auth";
+import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
+import { useIsFocused } from "@react-navigation/native";
 
-import { Background } from "./styles";
-import Header from "../../components/Header";
 import api from "../../services/api";
+import Header from "../../components/Header";
+import BalanceItem from "../../components/BalanceItem";
+
+import { 
+  Background,
+  ListBalance
+} from "./styles";
 
 export default function Home() {
+  const isFocused = useIsFocused();
   const [listBalance, setListBalance] = useState([]);
-
   const [dateMovements, setDateMovements] = useState(new Date());
 
   useEffect(() => {
@@ -34,11 +38,19 @@ export default function Home() {
 
     return () => isActive = false;
 
-  }, [])
+  }, [isFocused])
 
   return(
     <Background>
       <Header title="Minhas Movimentações" />
+
+      <ListBalance 
+        data={listBalance}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={ item => item.tag }
+        renderItem={ ({ item }) => ( <BalanceItem data={item} /> )}
+      />
     </Background>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,3 +4,8 @@ export const Background = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
 `
+
+export const ListBalance = styled.FlatList`
+  max-height: 190px;
+  margin-top: 20px;
+`
